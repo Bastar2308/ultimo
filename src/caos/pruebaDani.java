a47c0aa8fc10eabec85a1ffac8b2881320c214e0
dani <dcast.ro@outlook.com>
2019-10-26 17:01:17 -0500

    cree cualquier cosa

---
+++ src/caos/pruebaDani.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package caos;
+
+/**
+ *
+ * @author bl4ck
+ */
+public class pruebaDani {
+    public static void main(String[] args) {
+        System.out.println("cualquier cosa");
+    }
+}
